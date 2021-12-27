2e5f913e81d6fc76cd9973a205a9c2eddbe52fd0
RomanKazakov1980 <swamper@yandex.ru>
2021-12-27 22:27:30 +0700

    mod; game_progress.py.py

--- brain_games/games/game_progress.py
+++ brain_games/games/game_progress.py
@@ -9,7 +9,7 @@ RANGE_COLLECTION = (0, 50)
 
 def ask_question_get_answer():
     step = random.randint(STEP_VALUE[0], STEP_VALUE[1])
-    start_collect = random.randint(RANGE_COLLECTION[0], RANGE_COLLECTION[10])
+    start_collect = random.randint(RANGE_COLLECTION[0], RANGE_COLLECTION[1])
     collection = list(range(start_collect, start_collect + step * 10, step))
     correct_answer = str(collection[step])
     collection[step] = '..'
